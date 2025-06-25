5c23bcd849cb9fc9ba5557dff442193f5258920d
Tyler Bisig <bisig1892@gmail.com>
2025-06-24 21:01:30 -0700

    Items model, controller, and view created

---
+++ ItemsList/Controllers/ItemsController.cs
@@ -0,0 +1,14 @@
+﻿using ItemsList.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ItemsList.Controllers
+{
+    public class ItemsController : Controller
+    {
+        public IActionResult Overview()
+        {
+            var item = new Item() { Name = "bat" };
+            return View(item);
+        }
+    }
+}
